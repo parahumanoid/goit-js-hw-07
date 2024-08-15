c817712c2cffec7f0058045e078d9cf8fdd9c428
parahumanoid <57003748+parahumanoid@users.noreply.github.com>
2024-08-15 14:34:24 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+class Storage {
+    #items = [];
+
+    constructor(items) {
+        items.forEach(item => this.#items.push(item));
+    }
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        if (this.#items.includes(newItem)===false)
+            this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        const idx = this.#items.indexOf(itemToRemove);
+        if (idx >= 0) this.#items.splice(idx, 1);
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+console.log("--------------------END TASK 2--------------------");
